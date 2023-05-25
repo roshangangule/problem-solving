898cc0d1d685946bd0fc88be52d629c45d02b5f6
roshangangule <roshangangule@gmail.com>
2023-05-25 23:28:54 +0530

    Patterns Questions

--- src/main/java/com/problemSolving/patttern/Patterns.java
+++ src/main/java/com/problemSolving/patttern/Patterns.java
@@ -17,6 +17,8 @@ public class Patterns {
         rectTriangleNumberRevPattern(4);
         System.out.println();
         letterPattern(4);
+        System.out.println();
+        letterIncrePattern(4);
     }
 
     private static void numberPattern(int number) {
@@ -86,6 +88,17 @@ public class Patterns {
             for ( int j = 1; j <= number; j++) {
                 char ch = (char) ('A'+i-1);
                 System.out.print(" "+ch);
+            }
+            System.out.println();
+        }
+    }
+    private static void letterIncrePattern(int number) {
+        int count = 0;
+        for ( int i = 1; i <= number; i++) {
+            for ( int j = 1; j <= number; j++) {
+                char ch = (char) ('A'+count);
+                System.out.print(" "+ch);
+                count++;
             }
             System.out.println();
         }
