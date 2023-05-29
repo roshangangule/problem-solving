a3ab3b5efbd34c62eae894f9c1af724df00c8be9
roshangangule <roshangangule@gmail.com>
2023-05-29 22:44:44 +0530

    bitwise operator questions

---
+++ src/main/java/com/problemSolving/bitwise/BitwiseOperator.java
@@ -0,0 +1,14 @@
+package com.problemSolving.bitwise;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+
+        System.out.println("a & b = "+ (a&b));
+        System.out.println("a | b = "+ (a|b));
+        System.out.println("~a = "+ (~a));
+        System.out.println("a ^ b = "+ (a^b));
+
+    }
+}
